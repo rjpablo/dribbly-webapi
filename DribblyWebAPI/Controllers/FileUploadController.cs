2ddeccb1faca5b22fea94bb537155f85e3054a69
RJ Pablo <numerianop@sedata.com>
2017-03-19 09:34:42 +0800

    Added function to delete uploaded files

--- DribblyWebAPI/Controllers/FileUploadController.cs
+++ DribblyWebAPI/Controllers/FileUploadController.cs
@@ -8,41 +8,72 @@ using System.IO;
 
 namespace DribblyWebAPI.Controllers
 {
-    public class FileUploadController : ApiController
+    public class FileController : ApiController
     {
-        public string Upload()
+        private string uploadPath = "D:/RJ/Projects/dribbly-test/www/images/uploads/courts/";
+
+        [Route("api/file/upload")]
+        public IHttpActionResult Upload()
         {
-            string uploadPath = "D:/RJ/Projects/dribbly-test/www/images/uploads/courts/";
+            
             string uploadedFilePath = "";
 
             System.Web.HttpFileCollection files = System.Web.HttpContext.Current.Request.Files;
 
             try
             {
-                int uploadCount = 0;
+                string ext = System.IO.Path.GetExtension(files[0].FileName);
+                string uploadedFileName;
 
-                for (int i = 0; i < files.Count; i++)
+                do
                 {
+                    uploadedFileName = DateTime.Now.ToString("yyyyMMddHHmmssfffffff") + ext;
+                    uploadedFilePath = uploadPath + uploadedFileName;
+                } while (File.Exists(uploadedFilePath));
 
-                    System.Web.HttpPostedFile currentFile = files[i];
+                
+                files[0].SaveAs(uploadedFilePath);
 
-                    if (currentFile.ContentLength > 0)
-                    {
-                        uploadedFilePath = uploadPath + Path.GetFileName(currentFile.FileName);
-                        currentFile.SaveAs(uploadedFilePath);
-                        uploadCount++;
+                return Ok(uploadedFileName);
 
-                    }
+                // int uploadCount = 0;
 
-                }
+                //for (int i = 0; i < files.Count; i++)
+                //{
 
-                return uploadedFilePath;
+                //    System.Web.HttpPostedFile currentFile = files[i];
+
+                //    if (currentFile.ContentLength > 0)
+                //    {
+                //        uploadedFilePath = uploadPath + Path.GetFileName(currentFile.FileName);
+                //        currentFile.SaveAs(uploadedFilePath);
+                //        uploadCount++;
+
+                //    }
+
+                //}
             }
             catch (Exception ex)
             {
-                return "Upload failed with error '" + ex.Message + "'";
+                return InternalServerError(ex);
             }
             
         }
+
+        [Route("api/file/delete")]
+        public IHttpActionResult delete()
+        {
+            try
+            {
+                string fileName = System.Web.HttpContext.Current.Request.Params["filename"].ToString();
+                File.Delete(uploadPath + fileName);
+                return Ok("");
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }            
+        }
+
     }
 }
